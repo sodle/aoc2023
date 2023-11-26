845b41b8ca02366f56c27cec48b848a491ae61b0
Scott Odle <scott@sjodle.com>
2023-11-26 13:19:22 -0700

    refactor tests

--- src/test/kotlin/TestDay01.kt
+++ src/test/kotlin/TestDay01.kt
@@ -2,9 +2,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class TestDay01 {
+    private val puzzle = Day01()
+
     @Test
     fun testGetInput() {
-        val puzzle = Day01()
         assertThrows<NotImplementedError> {
             puzzle.getPuzzleInput()
         }
@@ -12,7 +13,6 @@ class TestDay01 {
 
     @Test
     fun testPart1() {
-        val puzzle = Day01()
         assertThrows<NotImplementedError> {
             puzzle.part1()
         }
@@ -20,7 +20,6 @@ class TestDay01 {
 
     @Test
     fun testPart2() {
-        val puzzle = Day01()
         assertThrows<NotImplementedError> {
             puzzle.part2()
         }
