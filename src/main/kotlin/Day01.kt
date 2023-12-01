74d76b2a83f0057e8a92757ea5444b22ddda3102
Scott Odle <scott@sjodle.com>
2023-11-30 23:29:39 -0700

    day 1 complete

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,4 +1,70 @@
-class Day01: BasePuzzle<List<Int>, Int, Int>() {
+class Day01: BasePuzzle<List<String>, Int, Int>() {
+    override fun getPuzzleInput(): List<String> {
+        return readLines("day01.txt").filter { it.isNotBlank() }
+    }
+
+    override fun part1(input: List<String>): Int {
+        return input.map { line ->
+            line.filter { it.isDigit() }
+        }.map { line ->
+            line.first().toString() + line.last().toString()
+        }.sumOf { line ->
+            line.toInt()
+        }
+    }
+
+    private fun findDigitName(input: String): Int {
+        if (input.isBlank())
+            return 0
+
+        if (input.startsWith("one"))
+            return 1
+        if (input.startsWith("two"))
+            return 2
+        if (input.startsWith("three"))
+            return 3
+        if (input.startsWith("four"))
+            return 4
+        if (input.startsWith("five"))
+            return 5
+        if (input.startsWith("six"))
+            return 6
+        if (input.startsWith("seven"))
+            return 7
+        if (input.startsWith("eight"))
+            return 8
+        if (input.startsWith("nine"))
+            return 9
+
+        if (input.first().isDigit())
+            return input.first().digitToInt()
+
+        return 0
+    }
+
+    override fun part2(input: List<String>): Int {
+        return input.sumOf {
+            var firstDigit = 0
+            var lastDigit = 0
+
+            for (i in it.indices) {
+                firstDigit = findDigitName(it.substring(i))
+                if (firstDigit > 0) {
+                    break
+                }
+            }
+
+            for (i in it.indices.reversed()) {
+                lastDigit = findDigitName(it.substring(i))
+                if (lastDigit > 0) {
+                    break
+                }
+            }
+
+            firstDigit * 10 + lastDigit
+        }
+    }
+
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             val dayClass = this::class.java.declaringClass.getDeclaredConstructor()
--- src/test/kotlin/TestDay01.kt
+++ src/test/kotlin/TestDay01.kt
@@ -1,27 +1,41 @@
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.Assertions.*
 
 class TestDay01 {
     private val puzzle = Day01()
 
     @Test
     fun testGetInput() {
-        assertThrows<NotImplementedError> {
-            puzzle.getPuzzleInput()
-        }
+        val input = puzzle.getPuzzleInput()
+        val expected = listOf(
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet",
+        )
+        assertLinesMatch(expected, input)
     }
 
     @Test
     fun testPart1() {
-        assertThrows<NotImplementedError> {
-            puzzle.part1()
-        }
+        val expected = 142
+        val actual = puzzle.part1()
+        assertEquals(expected, actual)
     }
 
     @Test
     fun testPart2() {
-        assertThrows<NotImplementedError> {
-            puzzle.part2()
-        }
+        val expected = 281
+        val actual = puzzle.part2(listOf(
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+        ))
+        assertEquals(expected, actual)
     }
 }
